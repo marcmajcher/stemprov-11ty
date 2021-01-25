23d5ee9a627ec06efa4a5bc87f5754dd7eac707f
Marc Majcher <majcher@gmail.com>
2021-01-25 17:50:32 -0600

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -106,25 +106,12 @@ module.exports = function(eleventyConfig) {
       "liquid"
     ],
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
-
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
-
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
-
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
 
-    // These are all optional, defaults are shown:
     dir: {
       input: "./src",
-      includes: "_includes",
-      data: "_data",
       output: "docs"
     }
   };
